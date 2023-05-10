60bd3737ac122d8380ffba9fb3dc2766e4197568
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-05-10 20:56:11 +0900

    [add]Step2
    ステージを回転させようまで完成

--- Assets/Scripts/StageController.cs
+++ Assets/Scripts/StageController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class StageController : MonoBehaviour
 {
+	public GameObject player;
+	Vector3 pivotDirection;
+	public float rotateSpeed = 3.0f;
+
 	// Use this for initialization
 	void Start()
 	{
-		
+		pivotDirection = new Vector3(0, 0, 1);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.RotateAround(player.transform.position, pivotDirection, -rotateSpeed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+            transform.RotateAround(player.transform.position, pivotDirection, rotateSpeed);
+        }
 	}
 }
