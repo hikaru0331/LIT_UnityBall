7955c1107250c30f4fc0b41a101a6c95eb5913eb
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-05-17 00:28:24 +0900

    [add]アレンジ2 ぬるり

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,17 +6,18 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public Vector3 additionalPosittion;
+    public Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = this.transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //カメラの座標をplayerの座標へadditionalPositionの座標を加算した座標に変更する
-        this.transform.position = player.transform.position + additionalPosittion;
+        this.transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, 3.0f * Time.deltaTime); 
     }
 }
