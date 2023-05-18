f01e323339f9395ec97dc390685a595dc2718215
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2023-05-19 02:59:42 +0900

    [add]アレンジ6 時間差で消える

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject restartPosition;
 
+    public Text objectName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectName.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log(other.gameObject.name);
+
+        objectName.gameObject.SetActive(true);
+        objectName.text = other.gameObject.name;
+
+        Invoke("DeleteText", 1.0f);
+
+    }
+
+    public void DeleteText()
+    {
+        objectName.gameObject.SetActive(false);
     }
 
     public void OnTriggerEnter(Collider other)
